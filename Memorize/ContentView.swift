5699a7807f1849d16e05a2940de97438ab996642
KC <kc995266@gmail.com>
2024-01-31 23:06:49 +0800

    [新增] Lecture 1 | Stanford CS193p 2023

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -2,21 +2,46 @@
 //  ContentView.swift
 //  Memorize
 //
-//  Created by Bruce Chen on 2024/1/31.
+//  Created by Bruce Chen on 2024/1/30.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        
+        HStack(content: {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        })
+        .foregroundColor(Color.orange)
         .padding()
     }
+}
+
+struct CardView: View {
+    
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻")
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+    
 }
 
 #Preview {
